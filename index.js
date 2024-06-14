3ad653b853481e6c69477912772c3c57b37bf0dd
yinethriv <karinariv0311@gmail.com>
2024-06-14 12:55:42 -0500

    netflix

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ document.getElementById("menu-btn").addEventListener("click", function(){
     const nav = document.getElementById("nav");
     nav.classList.toggle("open");
 })
+document.getElementById("menu-boton").addEventListener("click", function(){
+    const nav = document.getElementById("barra");
+    nav.classList.toggle("open");
+})
