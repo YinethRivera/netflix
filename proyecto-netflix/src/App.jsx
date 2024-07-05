9e5f5e9e25f20d1ca25a0a903eeec8c8c14ea263
yinethriv <karinariv0311@gmail.com>
2024-07-05 13:43:05 -0500

    netflix sesion78

--- proyecto-netflix/src/App.jsx
+++ proyecto-netflix/src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css"; 
+import { peliculas1, peliculas2, peliculas3 } from "./movieList.jsx"; 
+import Header from "./header.jsx"; 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header />
+
+      <div className="section">
+        <h3>Animadas</h3>
+        <div className="imagenes">
+          {peliculas1.map((pelicula) => (
+            <div key="pelicula" className="pelicula">
+              <img
+                className="imagen"
+                key={pelicula.id}
+                src={pelicula.imagen}
+                alt={pelicula.titulo}
+              />
+              <p className="titulo">{pelicula.titulo}</p>
+            </div>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="section">
+        <h3>Novelas </h3>
+        <div className="imagenes">
+          {peliculas2.map((pelicula) => (
+            <div key={pelicula.id} className="pelicula">
+              <img
+                className="imagen"
+                key={pelicula.id}
+                src={pelicula.imagen}
+                alt={pelicula.titulo}
+              />
+              <p className="titulo">{pelicula.titulo}</p>
+            </div>
+          ))}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="section">
+        <h3>Accion</h3>
+        <div className="imagenes">
+          {peliculas3.map((pelicula) => (
+            <div key={pelicula.id} className="pelicula">
+              <img
+                className="imagen"
+                key={pelicula.id}
+                src={pelicula.imagen}
+                alt={pelicula.titulo}
+              />
+              <p className="titulo">{pelicula.titulo}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- proyecto-netflix/src/movieList.jsx
+++ proyecto-netflix/src/movieList.jsx
@@ -1,261 +1,199 @@
-const peliculas = [
+const peliculas1 = [
   {
     id: 1,
-    titulo: "",
+    titulo: "Squid-game",
     imagen:
       "https://www.findelahistoria.com/web/wp-content/uploads/2021/12/el-juego-del-calamar-squid-game-serie-mejor-poster.jpg",
   },
 
   {
     id: 2,
-    titulo: "",
+    titulo: "Wednesday",
     imagen:
       "https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQWAJ68nKbSun1YQc4HdkX7Lk5Q2BRy8ZWOPiWiIFmuVece6NCorgfzsAkX8ZvkItQhsSDbLpBuog0Ux1EumB0cxzFKamXpcSNdu36Tdk1XgmJgITbQsi5QvJOdbGSihsS3Sr30ukw0r3R1bW3dFdiyaS.jpg?r=0a3",
   },
 
   {
     id: 3,
-    titulo: "",
+    titulo: "The-witcher",
     imagen:
       "https://static.posters.cz/image/350/posters/the-witcher-season-3-i177456.jpg",
   },
 
   {
     id: 4,
-    titulo: "",
+    titulo: "Vikings Vahalla",
     imagen:
       "https://images.squarespace-cdn.com/content/v1/581bd18003596e16cc905cad/1674693031719-WNUCN10W0YVOPDF4EEUR/Vikings+Valhalla.jpeg",
   },
 
   {
     id: 5,
-    titulo: "",
+    titulo: "Dark",
     imagen:
       "https://rukminim2.flixcart.com/image/850/1000/kyvvtzk0/poster/f/z/t/medium-dark-netflix-series-matte-finish-poster-urbanprint6423-original-imagbygfd5fzyjvh.jpeg?q=90&crop=false",
   },
 
   {
     id: 6,
-    titulo: "",
+    titulo: "Stranger things",
     imagen:
       "https://i0.wp.com/m.media-amazon.com/images/I/91FTgJZL-kL._AC_SL1500_.jpg?fit=%2C&ssl=1",
   },
 
   {
     id: 7,
-    titulo: "",
+    titulo: " The addams family ",
     imagen:
       "https://www.hogarmania.com/archivos/202310/peliculas-miedo-para-ninos-familia-addams-XxXx80.jpg",
   },
 
   {
     id: 8,
-    titulo: "",
+    titulo: "La casa de papel",
     imagen:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA-e7vs5jvzt9jAkdSALEYNwFQxplwkLBgHievk2OMZHFRHhjbiSzHpbEygRI1mUzfyD8&usqp=CAU",
   },
 
   {
     id: 9,
-    titulo: "",
+    titulo: "The society",
     imagen:
       "https://hips.hearstapps.com/hmg-prod/images/the-society-serie-netflix-poster-1556612614.jpg",
   },
 
   {
     id: 10,
-    titulo: "",
+    titulo: "Sex education",
     imagen:
       "https://www.educaciontrespuntocero.com/wp-content/uploads/2020/05/sed-768x1024.jpg",
-  },
-
+  }];
+ const peliculas2=[
   {
     id: 11,
-    titulo: "",
+    titulo: "Cyberpunk",
     imagen:
       "https://pbs.twimg.com/media/FZFHJOrVEAEno8-?format=jpg&name=900x900",
   },
 
   {
     id: 12,
-    titulo: "",
+    titulo: "Kimetsu no yaiba",
     imagen:
       "https://static0.cbrimages.com/wordpress/wp-content/uploads/2023/04/demon-slayer-anime-poster.jpg",
   },
   {
     id: 13,
-    titulo: "",
+    titulo: "Howl's moving castle",
     imagen:
       "https://i.pinimg.com/564x/69/cc/f4/69ccf4b3cb15a77b297c67a191dcb16d.jpg",
   },
   {
     id: 14,
-    titulo: "",
+    titulo: "Kengan azhura ",
     imagen:
       "https://pbs.twimg.com/media/Fr0MmOgX0AMwPD2?format=jpg&name=4096x4096",
   },
 
   {
     id: 15,
-    titulo: "",
+    titulo: "One piece",
     imagen:
       "https://i.pinimg.com/736x/1c/08/c0/1c08c0cd193454fa02d7e84c2cf73a45.jpg",
   },
   {
     id: 16,
-    titulo: "",
+    titulo: "Higs-Rise invasion",
     imagen:
       "https://i.pinimg.com/originals/b5/de/82/b5de828b6dc1c406eda7b96841a7b535.jpg",
   },
   {
     id: 17,
-    titulo: "",
+    titulo: "Death note",
     imagen:
       "https://i.blogs.es/2e6e33/donde-ver-death-note-espanol/650_1200.jpeg",
   },
   {
     id: 18,
-    titulo: "",
+    titulo: "Anohana",
     imagen:
       "https://occ-0-1723-1722.1.nflxso.net/dnm/api/v6/XsrytRUxks8BtTRf9HNlZkW2tvY/AAAABcvEUXtNFRBthcDmFXo8Lhc4L10J5s2WVkm9ipP6V_9fM5Jl5x8mmacyTnR8pj_Y2ZM3gaiwontqaMdQh7gG4cdELHgbILEQzg.jpg",
   },
   {
     id: 19,
-    titulo: "",
+    titulo: "Gangsta",
     imagen:
       "https://lh5.googleusercontent.com/proxy/LR1RXmK_o7WKcxeRBan7DvZGnd_VBwmUWrqzA0_Sf5nW6x2srYB5fAtiG0VV2-oZYyhAUlGEtkQYxxPDW9yTmaPD4pAaXBtep62sGH6Zww",
-  },
-
+  }
+  ];
+const peliculas3=[
   {
     id: 20,
-    titulo: "",
+    titulo: "Naruto",
     imagen: "https://somoskudasai.com/wp-content/uploads/2023/07/get.jpg",
   },
 
   {
     id: 21,
-    titulo: "",
+    titulo: "Madagascar",
     imagen:
       "https://www.eluniverso.com/resizer/6slLboL1pLukHymo0LltVWT7oGw=/447x670/smart/filters:quality(70)/cloudfront-us-east-1.images.arcpublishing.com/eluniverso/EIYZIKXZY5CB5D2O4UXNGKY24Y.jpg",
   },
   {
     id: 22,
-    titulo: "",
+    titulo: "Matilda",
     imagen:
       "https://www.educaciontrespuntocero.com/wp-content/uploads/2017/01/Matilda.jpg",
   },
 
   {
     id: 23,
-    titulo: "",
+    titulo: "Jefe en pañales",
     imagen:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSneG_hNtMUlrPFEc1sGvQskKMIw-aSCBOhhQ&s",
   },
 
   {
     id: 24,
-    titulo: "",
+    titulo: "Jefe en pañales 2",
     imagen:
       "https://lanacionar-la-nacion-ar-prod.cdn.arcpublishing.com/resizer/siyfRU5TJdUBFnya0Na7denMkP8=/210x300/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/ZEIRWVSUKNC5LGJ3BAM5S22G6Q.jpg",
   },
   {
     id: 25,
-    titulo: "",
+    titulo: "Los increibles",
     imagen: "https://cdn6.dibujos.net/images/listas/155/los-increibles.jpg",
   },
   {
     id: 26,
-    titulo: "",
+    titulo: "los increiblea 2 ",
     imagen:
       "https://es.web.img3.acsta.net/r_1280_720/pictures/18/05/11/10/41/4556004.jpg",
   },
   {
     id: 27,
-    titulo: "",
+    titulo: "Luvia de hamburguesas",
     imagen:
       "https://www.clara.es/medio/2022/11/11/lluvia-de-albondigas-3-a-6-anos_d8fa04c3_1000x1500.jpg",
   },
   {
     id: 28,
-    titulo: "",
+    titulo: "Mas allá de la luna ",
     imagen:
       "https://hips.hearstapps.com/hmg-prod/images/mas-alla-de-la-luna-1592979333.jpg",
   },
   {
     id: 29,
-    titulo: "",
+    titulo: "True",
     imagen:
       "https://www.fiebreseries.com/wp-content/uploads/2021/05/Vera-y-el-Reino-Arcoris_poster_serie0.jpg",
   },
   {
     id: 30,
-    titulo: "",
+    titulo: "Beat bugs",
     imagen: "https://cdn6.dibujos.net/images/listas/163/beat-bugs.jpg",
-  },
-
-  {
-    id: 31,
-    titulo: "",
-    imagen:
-      "https://www.digitaltrends.com/wp-content/uploads/2024/05/bcM2Tl5HlsvPBnL8DKP9Ie6vU4r.jpg?p=1#038;p=1.jpg",
-  },
-  {
-    id: 32,
-    titulo: "",
-    imagen: "https://images.justwatch.com/poster/251466363/s718/nightbooks.jpg",
-  },
-  {
-    id: 33,
-    titulo: "",
-    imagen:
-      "https://hips.hearstapps.com/hmg-prod/images/hyperdrive-1670940383.jpeg",
-  },
-  {
-    id: 34,
-    titulo: "",
-    imagen:
-      "https://hips.hearstapps.com/hmg-prod/images/cronicas-gallery-a-1542898327.jpg?crop=1xw:1xh;center,top&resize=640:*",
-  },
-  {
-    id: 35,
-    titulo: "",
-    imagen:
-      "https://i.pinimg.com/originals/1a/1b/ca/1a1bca448da1b6cdba689d70aaf38204.jpg",
-  },
-
-  {
-    id: 36,
-    titulo: "",
-    imagen:
-      "https://themantovanis.files.wordpress.com/2019/06/dentro-la-tormenta.jpg",
-  },
-
-  {
-    id: 37,
-    titulo: "",
-    imagen:
-      "https://m.media-amazon.com/images/S/pv-target-images/247667fc1db87f437dcbdfebccad42472804d134659861dae688f889d206b894.jpg",
-  },
-
-  {
-    id: 38,
-    titulo: "",
-    imagen:
-      "https://media.filmelier.com/tit/iZbNs5/poster/la-madre-de-la-novia_-d8JI28.jpeg",
-  },
-
-  {
-    id: 39,
-    titulo: "",
-    imagen:
-      "https://elcomercio.pe/resizer/NqNYHD-kPTyyTa4NGSSIKHqf200=/400x0/smart/filters:format(jpeg):quality(90)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/UGKBQF5CXFHQ7G5RXW426HMFEE.jpg",
-  },
-
-  {
-    id: 40,
-    titulo: "",
-    imagen:
-      "https://media.themoviedb.org/t/p/w500/hkxxMIGaiCTmrEArK7J56JTKUlB.jpg",
-  },
+  }
 ];
-export { peliculas };
+
+
+export { peliculas1, peliculas2, peliculas3, };
